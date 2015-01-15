5c9d00822b07f68e1969dc6cc646ec0fedd801a6
Chris Gratigny <chrisg@ibethel.org>
2015-01-14 21:26:22 -0800

    show help.html and remove htaccess

--- app.rb
+++ app.rb
@@ -8,8 +8,12 @@ class BethelGive < Sinatra::Application
     send_file File.expand_path("index.html", settings.public_folder)
   end
 
+  get "/help" do
+    send_file File.expand_path("help.html", settings.public_folder)
+  end
+
   not_found do
-    status 301
+    status 404
     redirect "/"
   end
 
